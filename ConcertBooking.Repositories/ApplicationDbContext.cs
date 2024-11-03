358d6b2c1ed44f3f80ddbeb70e88bcef107bead9
pranavgupta99 <pranavgupta547@gmail.com>
2024-11-03 15:57:08 +0530

    Concert Booking System

---
+++ ConcertBooking.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ConcertBooking.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearning.ConcertBooking.Repositories
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) :base(options)
+        {
+            
+        }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Concert> Concerts { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
