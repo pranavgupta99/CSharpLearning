5dc77de390387cebc86a32c55d2d5a3caca9d817
pranavgupta99 <pranavgupta547@gmail.com>
2024-11-15 22:28:02 +0530

    Sorting and Searching

--- CSharpLearning.UI/Controllers/SkillsController.cs
+++ CSharpLearning.UI/Controllers/SkillsController.cs
@@ -15,11 +15,36 @@ namespace CSharpLearning.UI.Controllers
             _skillRepo = skillRepo;
         }
 
-        public async Task<IActionResult> Index(int pageNumber =1, int pageSize =3)
+        public async Task<IActionResult> Index(string sortOrder, string filterText, int pageNumber =1, int pageSize =3, string searchText = null)
         {
             List<SkillViewModel> vm = new List<SkillViewModel>();
+            ViewData["SortFilter"] = sortOrder;
+            ViewData["IdSort"] = sortOrder == "Id_desc" ? "" : "Id_desc";
+            ViewData["TitleSort"] = sortOrder == "title_desc" ? "title_asc" : "title_desc";
             var skills = await _skillRepo.GetAll();
+            if(searchText!= null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchText = filterText;
+            }
+            ViewData["filterData"] = searchText; 
+
+            switch(sortOrder)
+            {
+                case "Id_desc": skills = skills.OrderByDescending(x => x.Id).ToList(); break;
+                case "title_desc": skills = skills.OrderByDescending(x => x.Title).ToList(); break;
+                case "title_asc": skills = skills.OrderBy(x => x.Title).ToList(); break;
+                default: skills = skills.OrderBy(x => x.Id).ToList(); break;
+            }
             int totalItems = 0;
+
+            if(!string.IsNullOrEmpty(searchText))
+            {
+                skills = skills.Where(x=>x.Title.Contains(searchText));
+            }
 
             totalItems =skills.ToList().Count;
             skills = skills.Skip((pageNumber-1) * pageSize).Take(pageSize).ToList();
